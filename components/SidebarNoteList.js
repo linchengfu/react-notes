02829f97ec327e3b90d4661025b255d2a6498b5f
chengfu <chengfu.lin@notta.ai>
2024-10-10 22:18:31 +0800

    fix: 解决循环引用问题

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -1,29 +1,31 @@
-import SidebarNoteList from '@/components/SidebarNoteList';
-import { getAllNotes } from '@/lib/redis';
-import { sleep } from '@/lib/utils';
-import SidebarNoteItemHeader from '@/components/SidebarNoteItemHeader';
+import SidebarNoteListFilter from "@/components/SidebarNoteListFilter";
+import SidebarNoteItemHeader from "@/components/SidebarNoteItemHeader";
+import { getAllNotes } from "@/lib/redis";
+import { sleep } from "@/lib/utils";
 
 export default async function NoteList() {
-
-  await sleep(2000)
-  const notes = await getAllNotes()
+  await sleep(2000);
+  const notes = await getAllNotes();
 
   if (Object.entries(notes).length == 0) {
-    return <div className="notes-empty">
-      {'No notes created yet!'}
-    </div>
+    return <div className="notes-empty">{"No notes created yet!"}</div>;
   }
 
   return (
-    <SidebarNoteList notes={
-      Object.entries(notes).map(([noteId, note]) => {
-        const noteData = JSON.parse(note)
+    <SidebarNoteListFilter
+      notes={Object.entries(notes).map(([noteId, note]) => {
+        const noteData = JSON.parse(note);
         return {
           noteId,
           note: noteData,
-          header: <SidebarNoteItemHeader title={noteData.title} updateTime={noteData.updateTime} />
-        }
-      })
-    } />
-  )
+          header: (
+            <SidebarNoteItemHeader
+              title={noteData.title}
+              updateTime={noteData.updateTime}
+            />
+          ),
+        };
+      })}
+    ></SidebarNoteListFilter>
+  );
 }
